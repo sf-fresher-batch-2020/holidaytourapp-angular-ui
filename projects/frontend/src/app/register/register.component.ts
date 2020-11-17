e32246da306effa0d5d8a2e0eeb8389ea2919a43
Saisushmithavk <46071629+Saisushmithavk@users.noreply.github.com>
2020-11-17 19:34:44 +0530

    updated

--- projects/frontend/src/app/register/register.component.ts
+++ projects/frontend/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private toastr: ToastrService, private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+  username: string;
+  email: string;
+  password: string;
+
+  register(form:NgForm){
+    
+    let formData={name:this.username,email:this.email, password: this.password, role:"USER", createdDate:new Date().toJSON()};
+    console.log(JSON.stringify(formData));
+    this.userService.register(formData).subscribe(res =>{
+      console.log(res);
+      this.toastr.success("Registered");
+      form.reset();
+      this.router.navigate['login']; // redirects to home
+    });
   }
 
 }
